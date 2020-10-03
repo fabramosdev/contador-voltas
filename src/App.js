8e1b58c3c2121448aa591cee2baca8a81fa2df32
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-10-03 16:17:34 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+const ShowLaps = (props) => {
+  return (
+    <p>
+      {props.voltas} <br />
+      Volta(s)
+    </p>
+  );
+};
+
+const ShowTime = (props) => {
+  const time = props.time;
+  const minutes = Math.round(time / 60);
+  const seconds = time % 60;
+  const minutesStr = minutes < 10 ? "0" + minutes : minutes;
+  const secondsStr = seconds < 10 ? "0" + seconds : seconds;
+
+  return (
+    <p>
+      {`${minutesStr}:${secondsStr}`} <br />
+      {props.text}
+    </p>
+  );
+};
+
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
 
 function App() {
+  const [numLaps, setNumLaps] = useState(0);
+  const [running, setRunning] = useState(false);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let timer = null;
+    if (running) {
+      timer = setInterval(() => {
+        setTime((old) => old + 1);
+      }, 1000);
+    }
+
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+    };
+  }, [running]);
+
+  const toggleRunning = () => {
+    setRunning(!running);
+  };
+
+  const increment = () => {
+    setNumLaps(numLaps + 1);
+  };
+
+  const decrement = () => {
+    setNumLaps(numLaps - 1);
+  };
+
+  const handleReset = () => {
+    setNumLaps(0);
+    setTime(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ShowLaps voltas={numLaps} />
+      <Button text="-" onClick={decrement} />
+      <Button text="+" onClick={increment} />
+      <ShowTime time={time} text="Tempo total" />
+      {numLaps > 0 && (
+        <ShowTime
+          time={Math.round(time / numLaps)}
+          text="Tempo medio por volta"
+        />
+      )}
+      <Button text="Iniciar" onClick={toggleRunning} />
+      <Button text="Reiniciar" onClick={handleReset} />
     </div>
   );
 }
